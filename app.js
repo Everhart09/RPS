6bc7b495aefb9c9e82e4f3dd257c0db721c4b1d2
Daniel Everhart <69944625+Everhart09@users.noreply.github.com>
2021-07-03 17:14:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-  let compChoice = Math.floor(Math.random() * 3)
+  let compChoice = Math.floor(Math.random() * 3);
   switch (compChoice) {
     case 0:
       return 'rock';
@@ -7,67 +7,68 @@ function computerPlay() {
       return 'paper';
     case 2:
       return 'scissors';
-  }
-}
+  };
+};
 
-let compScore = 0
-let userScore = 0
+let compScore = 0;
+let userScore = 0;
 
 function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === computerSelection) {
-    userScore = userScore
-    compScore = compScore
-    return 'This round is a tie, no points awarded.'
-  }
+    userScore = userScore;
+    compScore = compScore;
+    return 'This round is a tie, no points awarded.';
+  };
 
   if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-      compScore++
-      return 'The computer won this round, try again.'
+      compScore++;
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
-    }
-  }
+      userScore++;
+      return 'You won this round!';
+    };
+  };
 
   if (playerSelection === 'paper') {
     if (computerSelection === 'scissors') {
-      compScore++
-      return 'The computer won this round, try again.'
+      compScore++;
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
-    }
-  }
+      userScore++;
+      return 'You won this round!';
+    };
+  };
 
   if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
       compScore++
-      return 'The computer won this round, try again.'
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
-    }
-  }
-}
+      userScore++;
+      return 'You won this round!';
+    };
+  };
+};
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Make a choice: Rock, Paper, Scissors').toLowerCase()
-    const computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(`You picked ${playerSelection}, your score is: ${userScore}`)
-    console.log(`The computer picked ${computerSelection}, the computer's score is: ${compScore}`)
+    const playerSelection = prompt('Make a choice: Rock, Paper, Scissors').toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`You picked ${playerSelection}, your score is: ${userScore}`);
+    console.log(`The computer picked ${computerSelection}, the computer's score is: ${compScore}`);
 
-  }
+  };
+
   if (userScore > compScore) {
-    console.log('Congrats, you won the game!')
+    console.log('Congrats, you won the game!');
   } else if (userScore < compScore) {
-    console.log('The computer won the game, better luck next time.')
+    console.log('The computer won the game, better luck next time.');
   } else {
-    console.log('The game is a tie.')
-  }
-}
+    console.log('The game is a tie.');
+  };
+};
 
-game()
+game();
