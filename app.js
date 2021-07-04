b62248eb5ee43b6e22f596a2fd652d875495e080
Daniel Everhart <69944625+Everhart09@users.noreply.github.com>
2021-07-03 17:33:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,36 +28,36 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === computerSelection) {
 
-    return 'This round is a tie, no points awarded.'
+    return 'This round is a tie, no points awarded.';
   }
 
   if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-      compScore++
-      return 'The computer won this round, try again.'
+      compScore++;
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
+      userScore++;
+      return 'You won this round!';
     }
   }
 
   if (playerSelection === 'paper') {
     if (computerSelection === 'scissors') {
-      compScore++
-      return 'The computer won this round, try again.'
+      compScore++;
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
+      userScore++;
+      return 'You won this round!';
     }
   }
 
   if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
-      compScore++
-      return 'The computer won this round, try again.'
+      compScore++;
+      return 'The computer won this round, try again.';
     } else {
-      userScore++
-      return 'You won this round!'
+      userScore++;
+      return 'You won this round!';
     }
   }
 }
@@ -68,23 +68,23 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Make a choice: Rock, Paper, Scissors').toLowerCase()
-    const computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(`You picked ${playerSelection}, your score is: ${userScore}`)
-    console.log(`The computer picked ${computerSelection}, the computer's score is: ${compScore}`)
+    const playerSelection = prompt('Make a choice: Rock, Paper, Scissors').toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`You picked ${playerSelection}, your score is: ${userScore}`);
+    console.log(`The computer picked ${computerSelection}, the computer's score is: ${compScore}`);
 
   }
 
   if (userScore > compScore) {
-    console.log('Congrats, you won the game!')
+    console.log('Congrats, you won the game!');
   } else if (userScore < compScore) {
-    console.log('The computer won the game, better luck next time.')
+    console.log('The computer won the game, better luck next time.');
   } else {
-    console.log('The game is a tie.')
+    console.log('The game is a tie.');
   }
 }
 
-game()
+game();
 
 /*-----------------------------------------------------------------------------------------------------------------------------*/
